1e5c4c290e8501371007564e99578d6a22e4d24c
Fernando Laura Torrez <fernandolaura1726@gmail.com>
2026-07-07 21:37:17 -0400

    Desarrorollo termonado del backend con frontend tradicional

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,36 @@ app.get('/api/usuarios/:id', (req, res) => {
     res.json({ success: true, data: usuario });
 });
 
+// Endpoint POST para crear nuevo usuario
+app.post('/api/usuarios', (req, res) => {
+const { nombre, email } = req.body;
+    // Validar que llegaron los datos
+    if (!nombre || !email) {
+        return res.status(400).json({
+            success: false,
+            error: 'Nombre y email son requeridos'
+        });
+    }
+    // Validar formato de email (básico)
+    if (!email.includes('@') || !email.includes('.')) {
+        return res.status(400).json({
+        success: false,
+        error: 'Email inválido'
+        });
+    }// Crear nuevo usuario con ID automático
+    const nuevoId = usuarios.length > 0 ? Math.max(...usuarios.map(u => u.id)) + 1 : 1;
+    const nuevoUsuario = {
+        id: nuevoId,
+        nombre,
+        email
+    };
+    usuarios.push(nuevoUsuario);
+    res.status(201).json({
+        success: true,
+        data: nuevoUsuario
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor backend corriendo  en http://localhost:${PORT}`);
     console.log('Endpoints disponibles');
